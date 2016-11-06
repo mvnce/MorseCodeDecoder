4451834ce859276bc9e082ab4fcbfba87f946245
vincentsma <vincentsma@hotmail.com>
2016-11-06 17:51:53 -0500

    added unit test framework

---
+++ UnitTestProject1/SolidSignalTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace MorseCode.Tests
+{
+    [TestClass]
+    public class SolidSignalTest
+    {
+        [TestMethod]
+        public void shouldReturn()
+        {
+            List<String> expected = new List<string>();
+            expected.Add("E");
+
+
+            MorseCode decoder = new MorseCode();
+            Assert.AreEqual(expected, decoder.decode("."));
+        }
+    }
+}
